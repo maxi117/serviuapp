c95d21fd0849d08fbdbbd857da87bf03da327341
Maxi <maxisaias2009@gmail.com>
2024-05-10 23:29:36 -0400

    ´´

--- serviuApp/views.py
+++ serviuApp/views.py
@@ -1,16 +1,13 @@
-from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from serviuApp.forms import CreateSubsidioForm
 from serviuApp.models import Subsidio
 
 # Create your views here.
-@csrf_exempt
 def index(request):
     subsidios = Subsidio.objects.all()
     data = {'subsidios': subsidios}
     return render(request, 'serviuApp/index.html', data)
 
-@csrf_exempt
 def crear(request):
     form = CreateSubsidioForm()
     if request.method == 'POST' :
